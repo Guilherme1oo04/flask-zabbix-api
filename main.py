d8ebeebda60f105a16dc2b9f1b3e331348348a13
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-11-07 16:09:33 -0300

    update main code api

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ z_api.login(api_token=token)
 
 app = Flask(__name__)
 
+@app.route('/')
+def indexPage():
+    return {"status": "ok"}
+
 @app.route('/problems')
 def problems():
 
-    json_problems = []
-    
-    result_problems = z_api.problem.get(output='extend', filter={'value': 1})
-
-    for problem in result_problems:
-        json_problems.append({"problem": problem["name"], "timestamp": problem["clock"], "opdata": problem["opdata"]})
-
-    return json_problems
+    return controller.problems(z_api)
 
 @app.route("/lastTrafegos/<host>/out/<top>")
 def trafegosInterfacesOut(host, top):
